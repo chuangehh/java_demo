cd1353b5a73242cc4384c0f995cc2e6e1b4dd5c1
liangchuanchuan <liangchuanchuan@banggood.com>
2019-08-06 14:28:21 +0800

    jvm: 盘点家底查看JVM默认值

--- jvm/src/main/java/com/lcc/jvm/HelloGC.java
+++ jvm/src/main/java/com/lcc/jvm/HelloGC.java
@@ -2,6 +2,13 @@ package com.lcc.jvm;
 
 /**
  * VM options:-XX:+PrintGCDetails
+ *
+ * PrintFlagsFinal举例,运行java命令的同时打印出参数:
+ * VM options: -Xms1g -Xmx2g -XX:+PrintFlagsFinal -XX:MaxTenuringThreshold=10
+ * result:
+ * InitialHeapSize                          := 1073741824
+ * MaxHeapSize                              := 2147483648
+ * MaxTenuringThreshold                     := 10
  */
 public class HelloGC {
     public static void main(String[] args) throws InterruptedException {
