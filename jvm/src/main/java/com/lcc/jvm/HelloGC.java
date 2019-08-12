8bb7fa5dbb2cc6f5450eeb849a8bfce5f0114361
liangchuanchuan <liangchuanchuan@banggood.com>
2019-08-12 13:13:44 +0800

    jvm: 你平时工作用过的JVM常用基本配置参数有那些? JDK 1.8基础知识

--- jvm/src/main/java/com/lcc/jvm/HelloGC.java
+++ jvm/src/main/java/com/lcc/jvm/HelloGC.java
@@ -2,7 +2,7 @@ package com.lcc.jvm;
 
 /**
  * VM options:-XX:+PrintGCDetails
- *
+ * <p>
  * PrintFlagsFinal举例,运行java命令的同时打印出参数:
  * VM options: -Xms1g -Xmx2g -XX:+PrintFlagsFinal -XX:MaxTenuringThreshold=10
  * result:
@@ -11,9 +11,17 @@ package com.lcc.jvm;
  * MaxTenuringThreshold                     := 10
  */
 public class HelloGC {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("HelloGC");
-        Thread.sleep(Integer.MAX_VALUE);
-    }
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("HelloGC");
+		// -Xms
+		System.out.println("Runtime.getRuntime().totalMemory() MB: " + Runtime.getRuntime().totalMemory() / 1024 / 1024);
+		// -Xmx
+		System.out.println("Runtime.getRuntime().maxMemory() MB: " + Runtime.getRuntime().maxMemory() / 1024 / 1024);
+		// 当前可用申请的内存,
+		System.out.println("Runtime.getRuntime().freeMemory() MB: " + Runtime.getRuntime().freeMemory() / 1024 / 1024);
+		System.out.println("Runtime.getRuntime().availableProcessors(): " + Runtime.getRuntime().availableProcessors());
+
+		Thread.sleep(Integer.MAX_VALUE);
+	}
 
 }
