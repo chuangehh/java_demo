92cac065fde403c24148323ad2aa7f5ddc09c5b3
liangchuanchuan <liangchuanchuan@banggood.com>
2020-03-24 13:21:51 +0800

    mq: activemq 队列生产者,消费者小总结

--- mq/src/main/java/com/lcc/activemq/queue/JmsConsumer.java
+++ mq/src/main/java/com/lcc/activemq/queue/JmsConsumer.java
@@ -7,13 +7,18 @@ import javax.jms.*;
 import java.io.IOException;
 
 /**
- * 队列消息生产者
+ * 队列消息消费者
+ *
+ * 两种消费方式
+ * 1.同步阻塞方法 receive()
+ * 2.异步非阻塞方式(注册监听器,onMessage()), 监听器调用另一个线程处理
  */
 public class JmsConsumer {
 	public static final String DEFAULT_BROKER_URL = "tcp://hadoop100:61616";
 	public static final String QUEUE_NAME = "lcc_queue";
 
 	public static void main(String[] args) throws JMSException, IOException {
+		System.out.println("消费者1");
 		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(DEFAULT_BROKER_URL);
 		Connection connection = factory.createConnection("admin", "admin");
 		connection.start();
@@ -23,27 +28,31 @@ public class JmsConsumer {
 		MessageConsumer consumer = session.createConsumer(queue);
 
 		// 1.直接消费
-//		while (true) {
-//			Message receive = consumer.receive(200);
-//			if (receive instanceof ActiveMQTextMessage) {
-//				ActiveMQTextMessage textMessage = (ActiveMQTextMessage) receive;
-//				System.out.println("消费消息: " + textMessage.getText());
-//			}else{
-//				break;
-//			}
-//		}
+		while (true) {
+			Message receive = consumer.receive();
+			if (receive instanceof ActiveMQTextMessage) {
+				System.out.println(Thread.currentThread().getName() + " " );
+				ActiveMQTextMessage textMessage = (ActiveMQTextMessage) receive;
+				System.out.println("消费消息: " + textMessage.getText());
+			}else{
+				break;
+			}
+		}
+
+		System.out.println(Thread.currentThread().getName() + " " );
 
 		// 2.监听
-		consumer.setMessageListener(message -> {
-			if (message instanceof ActiveMQTextMessage) {
-				ActiveMQTextMessage textMessage = (ActiveMQTextMessage) message;
-				try {
-					System.out.println("消费消息: " + textMessage.getText());
-				} catch (JMSException e) {
-					e.printStackTrace();
-				}
-			}
-		});
+//		consumer.setMessageListener(message -> {
+//			if (message instanceof ActiveMQTextMessage) {
+//				System.out.println(Thread.currentThread().getName() + " " );
+//				ActiveMQTextMessage textMessage = (ActiveMQTextMessage) message;
+//				try {
+//					System.out.println("消费消息: " + textMessage.getText());
+//				} catch (JMSException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
 
 		System.in.read();
 		session.close();
--- mq/src/main/java/com/lcc/activemq/queue/JmsProduce.java
+++ mq/src/main/java/com/lcc/activemq/queue/JmsProduce.java
@@ -5,7 +5,22 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import javax.jms.*;
 
 /**
+ * 队列消息生产者
+ * <p>
+ * 1.每条消息只能有一条消息
+ * 2.生产者与消费者没有时间上的相关性,无论消费者是否在线,消息都可以被提取
+ * 3.不会重复消费已有的消息
  *
+ *
+ * JMS开发步骤:
+ * 1.创建工厂
+ * 2.工厂创建连接
+ * 3.连接创建Session
+ * 4.Session创建 目的地(Topic,Queue)
+ * 5.Session创建 消费者/生产者
+ * 6.Session创建 消息
+ * 7.发送消息/消费消息
+ * 8.关闭相关资源
  */
 public class JmsProduce {
 	public static final String DEFAULT_BROKER_URL = "tcp://hadoop100:61616";
@@ -21,7 +36,7 @@ public class JmsProduce {
 		Queue queue = session.createQueue(QUEUE_NAME);
 
 		MessageProducer producer = session.createProducer(queue);
-		for (int i = 0; i < 10; i++) {
+		for (int i = 1; i <= 10; i++) {
 			producer.send(session.createTextMessage("hello activemq: " + i));
 		}
 
